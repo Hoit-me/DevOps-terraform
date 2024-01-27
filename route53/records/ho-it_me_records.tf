bd125a38fd2b2255ae4550581f1b84781f0a5a82
Denovo <vo0v0000@naver.com>
2024-01-28 05:47:24 +0900

    feat: route53 records - dev medicine search server

--- route53/records/ho-it_me_records.tf
+++ route53/records/ho-it_me_records.tf
@@ -1,7 +1,7 @@
 # #################### Route53 Record
-resource "aws_route53_record" "api-server" {
+resource "aws_route53_record" "dev_medicine_search_server" {
   zone_id        = var.r53_variables.ho-it_me_zone_id
-  name           = "api"
+  name           = "dev.search"
   type           = "A"
   set_identifier = var.AWS_REGION
 
@@ -15,92 +15,3 @@ resource "aws_route53_record" "api-server" {
     evaluate_target_health = true
   }
 }
-
-resource "aws_route53_record" "notification-server" {
-  zone_id        = var.r53_variables.ho-it_me_zone_id
-  name           = "notification"
-  type           = "A"
-  set_identifier = var.AWS_REGION
-
-  latency_routing_policy {
-    region = var.AWS_REGION
-  }
-
-  alias {
-    name                   = data.terraform_remote_state.alb.outputs.ext_alb.dns_name
-    zone_id                = data.terraform_remote_state.alb.outputs.ext_alb.zone_id
-    evaluate_target_health = true
-  }
-}
-
-resource "aws_route53_record" "er-front" {
-  zone_id        = var.r53_variables.ho-it_me_zone_id
-  name           = "er"
-  type           = "A"
-  set_identifier = var.AWS_REGION
-
-  latency_routing_policy {
-    region = var.AWS_REGION
-  }
-
-  alias {
-    name                   = data.terraform_remote_state.alb.outputs.ext_alb.dns_name
-    zone_id                = data.terraform_remote_state.alb.outputs.ext_alb.zone_id
-    evaluate_target_health = true
-  }
-}
-
-resource "aws_route53_record" "ems-front" {
-  zone_id        = var.r53_variables.ho-it_me_zone_id
-  name           = "ems"
-  type           = "A"
-  set_identifier = var.AWS_REGION
-
-  latency_routing_policy {
-    region = var.AWS_REGION
-  }
-  alias {
-    name                   = data.terraform_remote_state.alb.outputs.ext_alb.dns_name
-    zone_id                = data.terraform_remote_state.alb.outputs.ext_alb.zone_id
-    evaluate_target_health = true
-  }
-}
-
-resource "aws_route53_record" "jenkins" {
-  zone_id        = var.r53_variables.ho-it_me_zone_id
-  name           = "jenkins"
-  type           = "A"
-  set_identifier = var.AWS_REGION
-
-  latency_routing_policy {
-    region = var.AWS_REGION
-  }
-  alias {
-    name                   = data.terraform_remote_state.alb.outputs.ext_alb.dns_name
-    zone_id                = data.terraform_remote_state.alb.outputs.ext_alb.zone_id
-    evaluate_target_health = true
-  }
-}
-
-
-
-#######################
-
-# resource "aws_route53_record" "external_dns" {
-#   count          = var.kafka_broker_count
-#   zone_id        = var.route53_external_zone_id
-#   name           = "$kafka${count.index + 1}"
-#   type           = "A"
-#   set_identifier = var.AWS_REGION
-
-#   latency_routing_policy {
-#     region = var.AWS_REGION
-#   }
-
-#   alias {
-#     name                   = data.terraform_remote_state.nlb.outputs.ext_nlb.dns_name
-#     zone_id                = data.terraform_remote_state.nlb.outputs.ext_nlb.zone_id
-#     evaluate_target_health = false
-#   }
-#   depends_on = [aws_lb.external]
-# }
